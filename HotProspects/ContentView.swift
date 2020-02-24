c96e70a84dee343f8fd692017416d8f2120a38e2
Davron <devrannur@gmail.comn>
2020-02-23 21:25:17 -0600

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Davron on 2/23/20.
+//  Copyright © 2020 Davron. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
